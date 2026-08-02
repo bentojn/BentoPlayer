e4b8f44d6356c74dec4a6d283b720a27092f6799
BentoTheCoder <theoclevino@gmail.com>
2026-08-02 13:44:42 -0300

    Update Form2.vb

--- Form2.vb
+++ Form2.vb
@@ -1,7 +1,7 @@
 Public Class Form2
 
     Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Process.Start("https://retroshow.su")
+        Process.Start("https://github.com/bentojn/BentoPlayer")
     End Sub
 
 End Class
